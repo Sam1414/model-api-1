00e9b3a2be65614625f06488598146ebb4c81fb8
Sam1414 <samarthagarwal1414@gmail.com>
2020-07-15 15:13:13 +0530

    l

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from Load_Model_Locally import build, initialize
-
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/url/')
 def get_res():
     # Getting Input
     url = request.args.get('url')
@@ -13,6 +12,11 @@ def get_res():
     result = build(url)
     # Returning Result
     return result
+
+
+@app.route('/')
+def default_page():
+    return '<html><body><h3>Enter URL in the following format:</h3><p><b>http://127.0.0.1:5000</b><i>/url/?url=</i><u>https://www.your_news_link.com</u></p></body></html>'
 
 
 if __name__ == '__main__':
